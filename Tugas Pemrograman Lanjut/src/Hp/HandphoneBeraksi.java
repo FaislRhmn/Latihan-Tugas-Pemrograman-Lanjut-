e54bc487327178a9c684018bf3920a0d8aa210b5
FaislRhmn <80962278+FaislRhmn@users.noreply.github.com>
2021-03-20 08:37:50 +0700

    Add files via upload

---
+++ Tugas Pemrograman Lanjut/src/Hp/HandphoneBeraksi.java
@@ -0,0 +1,15 @@
+
+package Hp;
+
+public class HandphoneBeraksi {
+    public static void main (String []args){
+        Handphone handphoneku = new Handphone();
+        
+        handphoneku.hidupkan="...";
+        handphoneku.lakukanPanggilan="...panggilan dilakukan";
+        handphoneku.kirimSMS="...SMS berhasil terkirim";
+        handphoneku.matikan="...";
+        handphoneku.keadaanHandphone();
+                
+    }
+}
